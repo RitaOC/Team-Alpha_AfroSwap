df8211d8f5c30d347d8486b6a94621693f17b86e
oma131 <anosikeihuoma@gmail.com>
2022-11-22 21:07:23 +0100

    new commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import SwapPage from './SwapPage';
+
+function App() {
+
+  return (
+    <div className="App">
+      <SwapPage />
+    </div>
+  )
+}
+
+export default App;
