a7bbe33eaf00eb56a11ba51b37563a0872b71a82
RitaOC <onwudiwechinwendu16@gmail.com>
2022-11-29 14:54:54 +0100

    contact-page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,64 +6,65 @@ function Contact() {
   return (
     <>
       <Navbar />
-      <section>
-        <div>
-          <h3>How can we help?</h3>
-          <p>
-            Kindly fill the form to get started,we will respond as soon as we
-            can.
-          </p>
-          <form action="" method="post" enctype="text/plain">
-            <table>
-              <tr>
-                <td>
-                  <label for="name">Full Name:</label>
-                </td>
-                <td>
-                  <input type="text" placeholder="Mary Jane" />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label for="tel">Phone number:</label>
-                </td>
-                <td>
-                  <input type="number" placeholder="+234xxxxxxxxxx" id="" />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label for="subject">Subject:</label>
-                </td>
-                <td>
-                  <input type="text" placeholder="Inquiry" />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label for="email">Email:</label>
-                </td>
-                <td>
-                  <input type="email" placeholder="xxx@gmail.com" id="" />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label for="name">How can we help you:</label>
-                </td>
-                <td>
-                  <textarea
-                    name="comment"
-                    placeholder="Leave your message"
-                    id=""
-                    cols="30"
-                    rows="10"
-                    required
-                  ></textarea>
-                </td>
-              </tr>
-            </table>
-            <button type="submit"></button>
+      <section id="Contact">
+        <div id="contact-card">
+          <div className="contact-card-a">
+            <h3>How can we help?</h3>
+            <p className="contact-instruction">
+              Kindly fill the form to get started,we will respond as soon as we
+              can.
+            </p>
+          </div>
+
+          <form className="contact-card-b">
+            <div class="form-group">
+              <label for="inputAddress">Full Name</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputAddress"
+                placeholder="Mary Jane"
+              ></input>
+            </div>
+            <div class="form-group">
+              <label for="inputAddress">Phone number</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputAddress"
+                placeholder="+234xxxxxxxxxx"
+              ></input>
+            </div>
+            <div class="form-group">
+              <label for="inputAddress">Subject</label>
+              <input
+                type="text"
+                class="form-control"
+                id="inputAddress"
+                placeholder="Inquiry"
+              ></input>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              ></input>
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">How can we help?</label>
+              <textarea
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+
+            <button id="contact-btn" type="submit" class="btn btn-primary">
+              Send
+            </button>
           </form>
         </div>
       </section>
